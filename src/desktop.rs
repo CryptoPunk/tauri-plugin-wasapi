a115b73b8dab626774a927200b35e8559a91349b
Max Vohra <max@seattlenetworks.com>
2026-04-02 21:14:32 -0700

    feat: initialize tauri-plugin-wasapi project structure with example app and ping command

---
+++ src/desktop.rs
@@ -0,0 +1,22 @@
+use serde::de::DeserializeOwned;
+use tauri::{plugin::PluginApi, AppHandle, Runtime};
+
+use crate::models::*;
+
+pub fn init<R: Runtime, C: DeserializeOwned>(
+  app: &AppHandle<R>,
+  _api: PluginApi<R, C>,
+) -> crate::Result<Wasapi<R>> {
+  Ok(Wasapi(app.clone()))
+}
+
+/// Access to the wasapi APIs.
+pub struct Wasapi<R: Runtime>(AppHandle<R>);
+
+impl<R: Runtime> Wasapi<R> {
+  pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
+    Ok(PingResponse {
+      value: payload.value,
+    })
+  }
+}
---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+use tauri::{
+  plugin::{Builder, TauriPlugin},
+  Manager, Runtime,
+};
+
+pub use models::*;
+
+#[cfg(desktop)]
+mod desktop;
+#[cfg(mobile)]
+mod mobile;
+
+mod commands;
+mod error;
+mod models;
+
+pub use error::{Error, Result};
+
+#[cfg(desktop)]
+use desktop::Wasapi;
+#[cfg(mobile)]
+use mobile::Wasapi;
+
+/// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the wasapi APIs.
+pub trait WasapiExt<R: Runtime> {
+  fn wasapi(&self) -> &Wasapi<R>;
+}
+
+impl<R: Runtime, T: Manager<R>> crate::WasapiExt<R> for T {
+  fn wasapi(&self) -> &Wasapi<R> {
+    self.state::<Wasapi<R>>().inner()
+  }
+}
+
+/// Initializes the plugin.
+pub fn init<R: Runtime>() -> TauriPlugin<R> {
+  Builder::new("wasapi")
+    .invoke_handler(tauri::generate_handler![commands::ping])
+    .setup(|app, api| {
+      #[cfg(mobile)]
+      let wasapi = mobile::init(app, api)?;
+      #[cfg(desktop)]
+      let wasapi = desktop::init(app, api)?;
+      app.manage(wasapi);
+      Ok(())
+    })
+    .build()
+}
